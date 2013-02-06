49925c58ad90521e62911255cfe6567719858a59
Noam Lewis <lenoam@gmail.com>
2013-02-05 22:22:53 -0500

    Changed calculation to use dynamic programming (solves memory issues and runs faster)

--- test_search.hs
+++ test_search.hs
@@ -5,12 +5,13 @@ module Main where
 import Debug.Trace (traceShow)
 import Data.List (minimumBy, sortBy, genericLength)
 import Control.Monad ((=<<), liftM, liftM2)
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, isJust)
+import System.IO (hPutStrLn, stderr)
 
 a0 = 0
-b0 = 14
+b0 = 31
  
-costConstant = 1
+costConstant = 1000
 
 constantCost = const costConstant
 linearCost x = (x + 1) * costConstant
@@ -18,16 +19,21 @@ quadraticCost x = (linearCost x) * (linearCost x)
 
 cost = linearCost
 
-testTrees = makeTrees a0 $! b0
-test1 = head $ (sortByAverageCost constantCost) . map fromJust $ testTrees 
-test2 = head $ (sortByAverageCost linearCost) . map fromJust $ testTrees
-test3 = head $ (sortByAverageCost quadraticCost) . map fromJust $ testTrees
+--testTrees = makeTrees a0 $! b0
+-- test1 = head $ (sortByAverageCost constantCost) . map fromJust $ testTrees 
+-- test2 = head $ (sortByAverageCost linearCost) . map fromJust $ testTrees
+-- test3 = head $ (sortByAverageCost quadraticCost) . map fromJust $ testTrees
 --test4 = head $ (sortByAverageDepth) . map fromJust $ testTrees
-
+test1 = makeTrees constantCost a0 b0
+test2 = makeTrees linearCost a0 b0
+test3 = makeTrees quadraticCost a0 b0
+  
 printGraph x label = do
+  let rangeStr = "[" ++ show a0 ++ ", " ++ show b0 ++ "]"
+  hPutStrLn stderr $ "solving: " ++ label ++ " - " ++ rangeStr
   putStr "digraph G {\n"
   putStr $ tree2dot x 
-  putStr $ "labelloc=\"t\";label=\"" ++ label ++ "\";"
+  putStr $ "labelloc=\"t\";label=\"" ++ label ++ " - " ++ rangeStr ++ "\";"
   putStr "}\n"
   
 main = do
@@ -39,35 +45,34 @@ allPairs :: Monad m => m a -> m b -> m (a,b)
 allPairs = (=<<) . flip (liftM . flip (,))
 
 data Tree a = Leaf { getValue :: a } | Node { getRange :: (a,a,a), 
-                                              getLeft :: Maybe (Tree a), 
-                                              getRight :: Maybe (Tree a) }
+                                              getLeft :: Tree a, 
+                                              getRight :: Tree a }
             deriving (Show)
 
-makeTrees :: (Enum a, Eq a, Num a) => a -> a -> [Maybe (Tree a)]
-makeTrees a b = if a == b 
-                then [Just $ Leaf a]
-                else foldr ((++) . makeTree') [] [a..(b-1)]
-  where makeTree' x = map (Just . (uncurry $ Node (a,x,b))) 
-                      . uncurry allPairs 
-                      $ if (x == a) then ([Just $ Leaf a], rightTrees)
-                        else if (x == b) then (leftTrees, [Nothing])
-                             else (leftTrees, rightTrees)
-          where leftTrees = makeTrees a x
-                rightTrees = makeTrees (x+1) b
+--makeTrees :: (Enum a, Eq a, Num a) => (a -> a -> Maybe (Tree a)
+makeTrees cost a b = if a == b 
+                     then Leaf a
+                     else head . (sortByAverageCost cost) $ map makeTree' [a..(b-1)]
+  where makeTree' x = uncurry (Node (a,x,b))
+                      $ if (x == a) 
+                        then (Leaf a, rightTree)
+                        else (leftTree, rightTree)
+          where leftTree = makeTrees cost a x
+                rightTree = makeTrees cost (x+1) b
                 
                 
 depth :: (Num b, Ord b) => Tree t -> b
 depth (Leaf _) = 1 
-depth (Node _ l r) = 1 + max (maybe 0 depth $ l) (maybe 0 depth $ r)
+depth (Node _ l r) = 1 + max (depth l) (depth r)
                      
 --depths :: (Num b, Ord b, Fractional b) => Tree t -> b
 depths (Leaf _) = [1] 
 depths (Node _ l r) = addDepth l ++ addDepth r
-  where addDepth x = map (+1) (maybe [0] depths x) 
+  where addDepth x = map (+1) (depths x) 
 
 costs f t@(Leaf x) = [f x] 
 costs f t@(Node (_,x,_) l r) = addCost l ++ addCost r
-  where addCost y = map (+(f x)) (maybe [0] (costs f) y) 
+  where addCost y = map (+(f x)) ((costs f) y) 
 
 
 averageDepth tree = sum d / genericLength d
@@ -93,7 +98,7 @@ nodeLabel' Nothing = "bad"
 nodeLabel' (Just x) = nodeLabel x
 
 tree2dot t@(Leaf _) = nodeLabel t ++ ";\n"
-tree2dot t@(Node _ l r) = nodeLabel t ++ " -> " ++ nodeLabel' l ++ ";\n" ++ nodeLabel t ++ " -> " ++ nodeLabel' r ++ ";\n" ++ tree2dot' l ++ tree2dot' r 
+tree2dot t@(Node _ l r) = nodeLabel t ++ " -> " ++ nodeLabel l ++ ";\n" ++ nodeLabel t ++ " -> " ++ nodeLabel r ++ ";\n" ++ tree2dot l ++ tree2dot r 
 
 tree2dot' :: (Show a, RealFrac a) => Maybe (Tree a) -> String 
 tree2dot' t@Nothing = nodeLabel' t
