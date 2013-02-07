47728551b9231fdc075c9f0f83c218922f2f8292
Noam Lewis <lenoam@gmail.com>
2013-02-06 22:54:08 -0500

    Refactor cost functions to use type Cost, and let them depend on the subrange

--- test_search.hs
+++ test_search.hs
@@ -18,22 +18,31 @@ b0 = 15
 costConstant :: Double
 costConstant = 100
 
-constantCost :: a -> Double
-constantCost = const costConstant
+constantCost :: Cost a Double
+constantCost _ _ = const costConstant
 
-linearCost :: Double -> Double
-linearCost x = (x + 1) * costConstant
+type Cost a b = a -> a -> a -> b
 
-quadraticCost :: Double -> Double
-quadraticCost x = (linearCost x) * (linearCost x)
+linearCost :: Cost Double Double
+linearCost _ _ t = (t + 1) * costConstant
 
-reverseLinearCost :: Double -> Double
-reverseLinearCost = (((b0 - a0) * costConstant) -) . linearCost
+quadraticCost :: Cost Double Double
+quadraticCost x y t = l' * l'
+  where l' = linearCost x y t
 
-eggsCost :: Double -> Double
-eggsCost x = if x < a0 + (b0 - a0) / 2 then 0 else 1
+reverseLinearCost :: Cost Double Double
+reverseLinearCost x y t = (((b0 - a0) * costConstant) -) $ linearCost x y t
 
-printGraph :: (Fractional a, Ord a, Show a) => (Double -> a) -> String -> IO ()
+-- cost for the "throwing eggs off of different floors" problem
+-- if t < s then 0 else 1
+-- the expected cost is: P(t >= s | s in [x, y])
+-- which for uniform distribution is equal to: 
+-- (t - x + 1) / (y - x + 1)
+eggsCost :: Cost Double Double
+eggsCost x y t =  costConstant * (t - x + 1) / (y - x + 1)
+
+
+printGraph :: (Fractional a, Ord a, Show a) => Cost Double a -> String -> IO ()
 printGraph cost label = do
   let x = makeTrees cost a0 b0 
       rangeStr = spaceConcat ["[", show a0, ",", show b0, "]"]
@@ -68,7 +77,7 @@ instance NFData a => NFData (Tree a) where
   rnf (Leaf x) = rnf x
   rnf (Node rng l r) = rnf rng `seq` rnf l `seq` rnf r
 
-makeTrees :: (NFData t, Ord t, Enum t, Eq t, Fractional a, Num t, Ord a) => (t -> a) -> t -> t -> Tree t
+makeTrees :: (NFData t, Ord t, Enum t, Eq t, Fractional a, Num t, Ord a) => Cost t a -> t -> t -> Tree t
 makeTrees cost a b = if a == b 
                      then Leaf a
                      else head . (sortByAverageCost cost) $ map' makeTree' [a..(b-1)]
@@ -83,16 +92,15 @@ makeTrees cost a b = if a == b
                else map
                 
                 
-averageCost :: Fractional b => (t -> b) -> Tree t -> b
+averageCost :: Fractional b => Cost t b -> Tree t -> b
 averageCost cost tree = sum d / genericLength d
   where d = calcCosts cost tree
         
 sortWith :: Ord a1 => (a -> a1) -> [a] -> [a]
 sortWith f = sortBy $ \a b -> compare (f a) (f b)
 
-sortByAverageCost :: (Fractional a, Ord a) => (t -> a) -> [Tree t] -> [Tree t]
+sortByAverageCost :: (Fractional a, Ord a) => (t -> t -> t -> a) -> [Tree t] -> [Tree t]
 sortByAverageCost f = sortWith $ averageCost f
-
 
 depth :: (Num b, Ord b) => Tree t -> b
 depth (Leaf _) = 1 
@@ -104,10 +112,10 @@ depths (Node _ l r) = addDepth l ++ addDepth r
   where addDepth x = map (+1) (depths x) 
 
 -- given a cost function and a tree, returns a list of the costs of all nodes in the tree
-calcCosts :: Num b => (t -> b) -> Tree t -> [b]
-calcCosts cost (Leaf x) = [0] 
-calcCosts cost (Node (_,x,_) l r) = addCost l ++ addCost r
-  where addCost y = map (+(cost x)) (calcCosts cost y) 
+calcCosts :: Num b => Cost t b -> Tree t -> [b]
+calcCosts _    (Leaf _) = [0] 
+calcCosts cost (Node (x,t,y) l r) = addCost l ++ addCost r
+  where addCost subTree = map (+(cost x y t)) (calcCosts cost subTree) 
 
 
 averageDepth :: Fractional a => Tree t -> a
